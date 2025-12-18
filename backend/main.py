91a5b57caf0a53cc6dfd272a0555cc0e0d4d87af
nsulonen <niko.sulonen@proton.me>
2025-12-18 13:12:28 +0200

    jsonify

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return jsonify({"message": "Hello from the server!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
